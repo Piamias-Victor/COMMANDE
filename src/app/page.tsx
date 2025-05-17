c912d94a93c7a3bee4731506266b97719dd9c1a6
Piamias Victor <chocowalletdev@proton.me>
2025-05-17 06:33:10 +0200

    Statut des commandes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,6 @@ export default function Home() {
           
           <StatsSummary />
           
-          <OrderStatusSummary />
         </section>
         
         <section className="mt-6">
